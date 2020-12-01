66753fafdad026b38838b0b6026e04c55ad0efd7
Charles <46895814+CharlesAnz@users.noreply.github.com>
2020-12-01 12:51:33 -0600

    Added swiping controls and cleaned up movement

--- Android Mobile Game/Assets/PlayerController.cs
+++ Android Mobile Game/Assets/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    public SwipeMotion swipeControl;
     Rigidbody2D playerRB;
     Collider2D playerCollider;
+    RectTransform thisRect;
     bool moving;
     public int moves;
     Vector2 moveNormal;
@@ -18,19 +20,30 @@ public class PlayerController : MonoBehaviour
     {
         playerRB = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<Collider2D>();
+        thisRect = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (swipeControl.SwipeUp)
+            Move(1);
+        if (swipeControl.SwipeD)
+            Move(2);
+        if (swipeControl.SwipeR)
+            Move(3);
+        if (swipeControl.SwipeL)
+            Move(4);
+
         movesTxt.text = "Moves: " + moves;
     }
 
 
     public void Move(int direction)
     {
-        if (moving == true) return;
+        if (moving == true) { Debug.Log("Still Moving"); return; }
 
+        //Up = 1, Down = 2, Right = 3, Left = 4
         switch (direction)
         {
             case 1:
@@ -66,9 +79,29 @@ public class PlayerController : MonoBehaviour
         if (collision.collider.gameObject.tag == "Obstacle")
         {   
             playerRB.velocity = new Vector2(0, 0);
+            
+            float m_x = thisRect.localPosition.x;
+            float m_y = thisRect.localPosition.y;
             Debug.Log("Contact normal is: " + contact.normal);
-            Debug.Log("Move normal is: " + moveNormal);
+            Debug.Log("X pos at hit is: " + m_x);
+            Debug.Log("Y pos at hit is: " + m_y);
             moving = false;
+
+            if (contact.normal.x == -1)
+                m_x = m_x - 10;
+
+            else if (contact.normal.x == 1)
+                m_x = m_x + 10;
+
+            if (contact.normal.y == -1)
+                m_y = m_y - 10;
+
+            else if (contact.normal.y == 1)
+                m_y = m_y + 10;
+
+            thisRect.localPosition = new Vector3 (m_x, m_y, thisRect.localPosition.z);
+            Debug.Log("X pos after hit is: " + thisRect.localPosition.x);
+            Debug.Log("Y pos after hit is: " + thisRect.localPosition.y);
         }
 
     }
---
+++ Android Mobile Game/Assets/Scenes/SwipeMotion.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeMotion : MonoBehaviour
+{
+    private bool dragging = false;
+    private bool swipeL, swipeR, swipeUp, swipeD;
+    private Vector2 start, deltaSwipe;
+
+    private void Update()
+    {
+        swipeL = false;
+        swipeR = false;
+        swipeUp = false;
+        swipeD = false;
+
+        #region Standalone Inputs
+        if(Input.GetMouseButtonDown(0))
+        {
+            dragging = true;
+            start = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+            Reset();
+        }
+        #endregion
+
+        #region Mobile Inputs
+        if(Input.touches.Length > 0)
+        {
+            if(Input.touches[0].phase == TouchPhase.Began)
+            {
+                dragging = true;
+                start = Input.touches[0].position;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                dragging = false;
+                Reset();
+            }
+        }
+        #endregion
+
+        //calculate distance
+        deltaSwipe = Vector2.zero;
+        if (dragging)
+        {
+            if (Input.touches.Length > 0)
+                deltaSwipe = Input.touches[0].position - start;
+            else if (Input.GetMouseButton(0))
+                deltaSwipe = (Vector2)Input.mousePosition - start;
+        }
+
+        if (deltaSwipe.magnitude > 75)
+        {
+            //Direction
+            float x = deltaSwipe.x;
+            float y = deltaSwipe.y;
+            if (Mathf.Abs(x) > Mathf.Abs(y))
+            {
+                //Left or Right
+                if (x < 0)
+                    swipeL = true;
+                else
+                    swipeR = true;
+            }
+            else 
+            {
+                //Up or Down                
+                if (y < 0)
+                    swipeD = true;
+                else
+                    swipeUp = true;
+            }
+
+            Reset();
+        }
+    }
+
+    private void Reset()
+    {
+        start = deltaSwipe = Vector2.zero;
+        dragging = false;
+    }
+
+    public Vector2 DeltaSwipe { get => deltaSwipe; set => deltaSwipe = value; }
+    public bool SwipeL { get => swipeL; set => swipeL = value; }
+    public bool SwipeR { get => swipeR; set => swipeR = value; }
+    public bool SwipeUp { get => swipeUp; set => swipeUp = value; }
+    public bool SwipeD { get => swipeD; set => swipeD = value; }
+}
