058c3277265aaeadb1c2cfe972beef1e8a713fdc
Charles <46895814+CharlesAnz@users.noreply.github.com>
2020-12-09 06:27:31 -0600

    Merged Gabi's Project

--- Android Mobile Game/Assets/EndScene.cs
+++ Android Mobile Game/Assets/EndScene.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndScene : MonoBehaviour
 {
     public GameObject EndScreen;
-
     public void EndGame()
     {
-        EndScreen.SetActive(true);
+        if (SceneManager.GetActiveScene().buildIndex < 5)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else { EndScreen.SetActive(true); }
 
     }
 
