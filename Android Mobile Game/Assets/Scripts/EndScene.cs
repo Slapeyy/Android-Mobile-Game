9cbca8cd264057aa92636cf19e4c29b65b0e10b3
Charles <46895814+CharlesAnz@users.noreply.github.com>
2020-12-16 16:33:01 -0600

    Changed End Scene, Added Gems, Player Character, and star rating system

---
+++ Android Mobile Game/Assets/Scripts/EndScene.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndScene : MonoBehaviour
+{
+    public GameObject EndScreen;
+    public Text StarRatingText;
+    int maxGems;
+    public int movesLeftForWin;
+
+    private void Start()
+    {
+        maxGems = GameObject.FindGameObjectsWithTag("Gem").Length;
+    }
+
+    public void EndGame(int gems, int moves)
+    {
+        if (SceneManager.GetActiveScene().buildIndex < 5)
+        {
+            EndScreen.SetActive(true);
+
+            if (gems == maxGems && moves >= movesLeftForWin)
+                StarRatingText.text = "You have finished the Tutorial Level with a GOLD star rating!";
+            else if (gems < maxGems && moves >= movesLeftForWin)
+                StarRatingText.text = "You have finished the Tutorial Level with a SILVER star rating!";
+            else if (gems < maxGems && moves < movesLeftForWin)
+                StarRatingText.text = "You have finished the Tutorial Level with a BRONZE star rating!";
+
+
+                            
+        }    
+    }
+
+    private void Update()
+    {
+        if(EndScreen.activeSelf == true && Input.GetKeyDown(KeyCode.Return))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            PlayerController player = collider.GetComponent<PlayerController>();
+            collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            EndGame(player.gems, player.moves);
+            player.Reset();
+        }
+    }
+
+}
---
+++ Android Mobile Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public SwipeMotion swipeControl;
+    public int moves;
+    public int gems;
+    public List<Sprite> player_anim;
+    public Text movesTxt;
+
+    Rigidbody2D playerRB;
+    Collider2D playerCollider;
+    RectTransform thisRect;
+    Image playerSprite;
+
+    bool moving;
+    Vector2 moveNormal;
+
+    LayerMask obstacleMask;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+        thisRect = GetComponent<RectTransform>();
+        playerSprite = GetComponent<Image>();
+        obstacleMask = LayerMask.GetMask("Obstacle");
+        movesTxt = GameObject.FindGameObjectWithTag("MovesTxt").GetComponent<Text>();
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (swipeControl.SwipeUp)
+            Move(1);
+        if (swipeControl.SwipeD)
+            Move(2);
+        if (swipeControl.SwipeR)
+            Move(3);
+        if (swipeControl.SwipeL)
+            Move(4);
+
+        if (playerCollider.IsTouchingLayers(obstacleMask)) 
+        {
+            float m_x = thisRect.localPosition.x;
+            float m_y = thisRect.localPosition.y;
+
+            if (playerSprite.sprite == player_anim[4])
+            {
+                m_x = m_x - 10;
+                playerSprite.sprite = player_anim[9];
+            }
+
+            else if (playerSprite.sprite == player_anim[2])
+            {
+                m_x = m_x + 10;
+                playerSprite.sprite = player_anim[6];
+            }
+
+            if (playerSprite.sprite == player_anim[11])
+            {
+                m_y = m_y - 10;
+                playerSprite.sprite = player_anim[5];
+            }
+
+            else if (playerSprite.sprite == player_anim[8])
+            {
+                m_y = m_y + 10;
+                playerSprite.sprite = player_anim[0];
+
+            }
+
+            thisRect.localPosition = new Vector3(m_x, m_y, thisRect.localPosition.z);
+
+        }
+        movesTxt.text = moves.ToString();
+    }
+
+
+    public void Move(int direction)
+    {
+        if (moving == true || moves <1) 
+        { 
+            Debug.Log("Still Moving"); 
+            return; 
+        }
+
+            //Up = 1, Down = 2, Right = 3, Left = 4
+        switch (direction)
+        {
+            case 1:
+                playerRB.velocity = new Vector2 (0, 150);
+                moveNormal = new Vector2 (0, -1);
+                playerSprite.sprite = player_anim[4];
+                break;
+            case 2:
+                playerRB.velocity = new Vector2(0, -150);
+                moveNormal = new Vector2(0, 1);
+                playerSprite.sprite = player_anim[2];
+                break;
+            case 3:
+                playerRB.velocity = new Vector2(150, 0);
+                moveNormal = new Vector2(-1, 0);
+                playerSprite.sprite = player_anim[11];
+                break;
+            case 4:
+                playerRB.velocity = new Vector2(-150, 0);
+                moveNormal = new Vector2(1, 0);
+                playerSprite.sprite = player_anim[8];
+                break;
+
+            default:
+                break;
+        }
+
+        moves--;
+        moving = true;
+    }
+
+    public void Reset()
+    {
+        moves = int.Parse(movesTxt.text);
+        moving = false;
+        gems = 0;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+
+        ContactPoint2D contact = collision.contacts[0];
+        if (collision.collider.gameObject.tag == "Obstacle")
+        {   
+            playerRB.velocity = new Vector2(0, 0);
+            
+            float m_x = thisRect.localPosition.x;
+            float m_y = thisRect.localPosition.y;
+            //Debug.Log("Contact normal is: " + contact.normal);
+            //Debug.Log("X pos at hit is: " + m_x);
+            //Debug.Log("Y pos at hit is: " + m_y);
+            moving = false;
+
+            if (contact.normal.x == -1)
+            {
+                m_x = m_x - 10;
+                playerSprite.sprite = player_anim[9];
+            }
+
+            else if (contact.normal.x == 1)
+            {
+                m_x = m_x + 10;
+                playerSprite.sprite = player_anim[6];
+            }
+
+            if (contact.normal.y == -1)
+            {
+                m_y = m_y - 10;
+                playerSprite.sprite = player_anim[5];
+            }
+
+            else if (contact.normal.y == 1)
+            {
+                m_y = m_y + 10;
+                playerSprite.sprite = player_anim[0];
+                
+            }
+
+            thisRect.localPosition = new Vector3 (m_x, m_y, thisRect.localPosition.z);
+            
+            //Debug.Log("Y pos after hit is: " + thisRect.localPosition.y);
+        }
+
+        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Gem")
+        {
+            gems++;
+            Destroy(collision.gameObject);
+                
+        }
+    }
+
+}
